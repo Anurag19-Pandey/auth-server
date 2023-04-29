b23e6c4df1fc887f570e3f111c5e13c9aba193c2
AnuragPandey-19 <anuragpandey192000@gmail.com>
2023-04-29 10:09:30 +0530

    Update googleCallbackRoute.js

--- routes/googleCallbackRoute.js
+++ routes/googleCallbackRoute.js
@@ -1,11 +1,9 @@
 const express = require("express") ;
 
-const { GoogleCallBack, VerificationRedirect} = require("../controllers/authControllers");
+const { GoogleCallBack} = require("../controllers/authControllers");
 
 const router = express.Router() ;
 
 router.route("/auth/google/callback").get(GoogleCallBack) ;
-
-router.route("/verify-email/:verificationString").get(VerificationRedirect) ;
 
 module.exports = router ;
